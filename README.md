81f98f49ab7ec5dac20f9d6eff68644f74f4f3b3
insua <insua@sohu.com>
2019-02-15 09:41:34 +0800

    feat(files):

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center"> weather-cli </h1>
+
+<p align="center"> an weather-cli to show icon and temperature.</p>
+
+
+## Installing
+
+```shell
+$ composer require insua/weather-cli -vvv
+```
+
+## Usage
+
+Use for i3blocks
+
+## Contributing
+
+You can contribute in one of three ways:
+
+1. File bug reports using the [issue tracker](https://github.com/insua/weather-cli/issues).
+2. Answer questions or fix bugs on the [issue tracker](https://github.com/insua/weather-cli/issues).
+3. Contribute new features or update the wiki.
+
+_The code contribution process is not very formal. You just need to make sure that you follow the PSR-0, PSR-1, and PSR-2 coding guidelines. Any new code contributions must be accompanied by unit tests where applicable._
+
+## License
+
+MIT
--- src/Weather.php
+++ src/Weather.php
@@ -2,7 +2,76 @@
 
 namespace Insua\WeatherCli;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use Sabre\Xml\Service;
+
 class Weather
 {
+    private $city;
 
+    /**
+     * Weather constructor.
+     * @param $city
+     * @throws \Sabre\Xml\ParseException
+     */
+    public function __construct($city)
+    {
+        $this->city = $city;
+        $this->getXml();
+    }
+
+    /**
+     * @throws \Sabre\Xml\ParseException
+     */
+    public function getXml()
+    {
+        $client = new Client();
+        try {
+            $response = $client->get('http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode='.$this->city,
+                ['timeout' => 0.5]);
+            $body = $response->getBody();
+            $this->getCurrentWeather((string)$body);
+        } catch (ConnectException $exception) {
+            echo ''.PHP_EOL;
+        }
+    }
+
+    /**
+     * @param $body
+     * @throws \Sabre\Xml\ParseException
+     */
+    public function getCurrentWeather($body)
+    {
+        $service = new Service();
+        $array = $service->parse($body);
+        $this->getWeather($array[0]['value'][8]['value'][0]['value']);
+    }
+
+    public function getWeather($current)
+    {
+        $array = explode(':', $current);
+        $icon = $this->getIcon(trim($array[1]));
+        echo $icon.$array[2].PHP_EOL;
+    }
+
+    public function getIcon($weather)
+    {
+        if(strpos('Cloudy', $weather) !== false || strpos('Dreary', $weather) !== false) {
+            return "";
+        } elseif (strpos('Sunny', $weather) !== false || strpos('Intermittent', $weather) !== false || strpos('Sunshine', $weather) !== false || strpos('Hot', $weather) !== false ) {
+            return '';
+        } elseif (strpos('Showers', $weather) !== false || strpos('T-Storms', $weather) !== false || strpos('Rain', $weather) !== false) {
+            return '';
+        } elseif (strpos('Windy', $weather) !== false) {
+            return '';
+        } elseif (strpos('Flurries', $weather) !== false || strpos('Snow', $weather) !== false || strpos('Ice', $weather) !== false || strpos('Sleet', $weather) !== false || strpos('Cold', $weather) !== false) {
+            return '';
+        } elseif (strpos('Clear', $weather) !== false || strpos('Moonlight', $weather) !== false) {
+            return '';
+        } elseif (strpos('Thunderstorms', $weather) !== false) {
+            return '';
+        }
+        return '';
+    }
 }
